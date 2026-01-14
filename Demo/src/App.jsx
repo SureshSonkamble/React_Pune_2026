8aa05c972d7807224ed12935f7dfdff6516a44f7
suresh sonkamble <suresh02021992@gmail.com>
2026-01-14 09:12:45 +0530

    14-01-2026

--- Demo/src/App.jsx
+++ Demo/src/App.jsx
@@ -1,8 +1,13 @@
+import Home from "./Home" //component
+import './App.css' //apply css
+import logo from './assets/hmm.jpg'; //set image from asset
 export default function App(){
   return(
-
 <>
-<h1>This Is React App</h1>
+<div class="d">
+<h1 id="h">This Is React App</h1>
+<img src={logo} height={100} width={250}></img>
+</div>
 </>
 
   )
--- Demo/src/Home.jsx
+++ Demo/src/Home.jsx
@@ -1,7 +1,15 @@
+import './Home.css'
 export default function Home(){
   return(
     <>
-    <h1>This is Home Page</h1>
+    <div class="d">
+    <h1 id="h">This is Home Page</h1>
+    <p id="h">Programming Language</p>
+    <ol>
+    <li>C</li><li>CPP</li>
+<li>JAVA</li><li>PYTHON</li><li>SQL</li>
+    </ol>
+    </div>
     </>
   )
 }
